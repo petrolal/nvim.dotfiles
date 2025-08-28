d0598108a28da5d927b8f3af530c274c11664d6a
petrolal <perolalucas@gmail.com>
2025-08-28 15:55:58 -0300

    feat:Added extra deafures

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,4 +1,4 @@
-local banner = {
+local banner = [[
   "████████╗ ██████╗ ██╗   ██╗██████╗ ██████╗ ████████╗███████╗██╗███╗   ███╗",
   "╚══██╔══╝██╔═══██╗██║   ██║██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██║████╗ ████║",
   "   ██║   ██║   ██║██║   ██║██████╔╝██████╔╝   ██║   █████╗  ██║██╔████╔██║",
@@ -6,7 +6,7 @@ local banner = {
   "   ██║   ╚██████╔╝╚██████╔╝██║  ██║██║  ██║   ██║   ███████╗██║██║ ╚═╝ ██║",
   "   ╚═╝    ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝╚═╝     ╚═╝",
   "                                                  [ トゥレット ヴィム ]",
-}
+]]
 
 return {
   -- add gruvbox
@@ -38,34 +38,6 @@ return {
         clangd = function(_, opts)
           opts.capabilities.offsetEncoding = { "utf-16" }
         end,
-      },
-    },
-  },
-
-  -- Snacks dashboard template
-  {
-    "snacks.nvim",
-    opts = {
-      dashboard = {
-        preset = {
-          pick = function(cmd, opts)
-            return LazyVim.pick(cmd, opts)()
-          end,
-          header = banner,
-        -- stylua: ignore
-        ---@type snacks.dashboard.Item[]
-        keys = {
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
-          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
-          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-        },
-        },
       },
     },
   },
@@ -123,6 +95,33 @@ return {
             opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
           end
         end,
+      },
+    },
+  },
+
+  {
+    "snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          pick = function(cmd, opts)
+            return LazyVim.pick(cmd, opts)()
+          end,
+          header = banner,
+        -- stylua: ignore
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+        },
       },
     },
   },
