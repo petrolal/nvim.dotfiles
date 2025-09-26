9fb2660482f097acf90d7fe8bade1021ec1612d3
petrolal <petrolalucas@gmail.com>
2025-09-26 18:15:25 -0300

    fix: codex

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -1,61 +1,27 @@
 return {
 
-  -- change some telescope options and a keymap to browse plugin files
+  -- tweak Telescope defaults and add a keymap for plugin files
   {
     "nvim-telescope/telescope.nvim",
-    keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
-    },
-    -- change some options
-    opts = {
-      defaults = {
+    keys = function(_, keys)
+      return vim.list_extend({
+        {
+          "<leader>fp",
+          function()
+            require("telescope.builtin").find_files({
+              cwd = require("lazy.core.config").options.root,
+            })
+          end,
+          desc = "Find Plugin File",
+        },
+      }, keys)
+    end,
+    opts = function(_, opts)
+      opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
         layout_strategy = "horizontal",
         layout_config = { prompt_position = "top" },
         sorting_strategy = "ascending",
         winblend = 0,
-      },
-    },
-  },
-
-  -- add more treesitter parsers
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-      },
-    },
-  },
-
-  -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
-  -- would overwrite `ensure_installed` with the new value.
-  -- If you'd rather extend the default config, use the code below instead:
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- add tsx and treesitter
-      vim.list_extend(opts.ensure_installed, {
-        "tsx",
-        "typescript",
       })
     end,
   },
