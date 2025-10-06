1b514195dfe4c601b77d6a9d5603e59363dd69ca
petrolal <petrolalucas@gmail.com>
2025-10-05 23:50:18 -0300

    fix: lsp python

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,9 +13,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 end
 vim.opt.rtp:prepend(lazypath)
-vim.g.python3_host_prog = vim.fn.exepath("python3")
-vim.g.lazyvim_python_lsp = "basedpyright"
-vim.g.lazyvim_python_ruff = "ruff"
 
 require("lazy").setup({
   spec = {
