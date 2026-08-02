5e42a7ca314e5649229b273ed22ce9a5fe6c3c7b
Petrolal <petrolalucas@gmail.com>
2026-08-02 17:46:29 -0300

    feature: applied keymaps for my spliy keyboard configuration

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,10 @@
 
 vim.g.have_nerd_font = true
 vim.g.lazyvim_colorscheme = "sentry-blu"
+
+-- Ergonomic keyboard settings (HRM, combos, mouse emulation)
+vim.opt.timeoutlen = 200
+vim.opt.ttimeoutlen = 10
+vim.opt.mouse = "a"
+vim.opt.relativenumber = true
+
